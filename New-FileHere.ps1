1fa0f33e1b6175766aca53c73c3c7281ad60440a
jdhitsolutions <jhicks@jdhitsolutions.com>
2016-03-30 08:25:31 -0400

    v3.3.1.2
    see changelog.txt

--- New-FileHere.ps1
+++ New-FileHere.ps1
@@ -8,13 +8,18 @@ Function New-FileHere {
 .Synopsis
 Create a new file in the current path.
 .Description
-When you create a new file, the ISE wants to create it in the directory you were in when you started the ISE. I often want to create a new file in the current location. This command has no parameters. You will be prompted for a new file name.
-
+When you create a new file, the ISE wants to create it in the directory you were in when you started the ISE. I often want to create a new file in the current location. This command has no parameters. You will be prompted for a new file name if you don't specify one.
+.Parameter Name
+Enter the name of the new file. If you dont' specify one you will be prompted.
 #>
 [cmdletbinding()]
-Param()
+Param(
+[string]$Name = (New-Inputbox -Prompt "Enter a file name" -Title "New File" -Default "MyUntitled.ps1"),
+[switch]$Open,
+[switch]$Passthru
+)
 
-[string]$name = New-Inputbox -Prompt "Enter a file name" -Title "New File" -Default "MyUntitled.ps1"
+
 if ($name -match "\w+") {
     $newpath = Join-path -Path (Get-Location).Path -ChildPath $name
     if (Test-Path -Path $newpath) {
@@ -22,11 +27,35 @@ if ($name -match "\w+") {
     }
     else {
         write-verbose "Adding $newFile"
-        "#requires -version $($PSVersionTable.PSVersion.tostring())" | Out-File -FilePath $newPath -NoClobber
-        $newfile = $psise.CurrentPowerShellTab.Files.add($newpath)
+        $head = @"
+#Requires -version $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)
+
+<#
+$Name
+
+  ****************************************************************
+  * DO NOT USE IN A PRODUCTION ENVIRONMENT UNTIL YOU HAVE TESTED *
+  * THOROUGHLY IN A LAB ENVIRONMENT. USE AT YOUR OWN RISK.  IF   *
+  * YOU DO NOT UNDERSTAND WHAT THIS SCRIPT DOES OR HOW IT WORKS, *
+  * DO NOT USE IT OUTSIDE OF A SECURE, TEST SETTING.             *
+  ****************************************************************
+#>
+
+
+"@
+        $head | Out-File -FilePath $newPath -NoClobber
+
+        #give the file a chance to be created
         start-sleep -Seconds 1
-        $newfile.editor.SetCaretPosition(2,1)
-        $newfile.editor.InsertText("`n")
+        
+        #Open the file 
+        if ($Open) {
+            psedit $newpath
+        }
+        if ($Passthru) {
+            Get-Item $newpath
+        }
+
     }
 }
 else {
