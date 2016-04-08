90ce4b2b53d5a93364cdf3e2453c7977da223ed3
Damien Solodow <bookwyrm79@gmail.com>
2016-04-08 17:22:38 -0400

    Minor cleanups
    reverted to upstream version
    cleaned up comment typo(s)
    improved consistency of capitalization

--- Sign-ISEScript.ps1
+++ Sign-ISEScript.ps1
@@ -1,6 +1,6 @@
 #requires -version 2.0
 
- 
+
 Function Write-Signature {
 
 Param()
@@ -8,16 +8,16 @@ Param()
 Set-StrictMode -Version Latest
 
 #get the certificate
-$cert = Get-ChildItem -Path cert:\currentuser\my -CodeSigningCert | Sort-Object NotAfter -Descending | Select-Object -First 1
+$cert = Get-ChildItem -Path Cert:\CurrentUser\My -CodeSigningCert
 if ($cert) {
 
     #save the file if necessary
-    if (!$psise.currentfile.IsSaved) {
+    if (!$psise.CurrentFile.IsSaved) {
      $psise.CurrentFile.Save()
     }
 
     #if the file is encoded as BigEndian, resave as Unicode
-    if ($psise.currentfile.encoding.encodingname -match "Big-Endian") {
+    if ($psise.CurrentFile.encoding.encodingname -match "Big-Endian") {
         $psise.CurrentFile.Save([Text.Encoding]::Unicode) | Out-Null
     }
 
@@ -28,16 +28,16 @@ if ($cert) {
     Try {
       Set-AuthenticodeSignature -FilePath $filepath -Certificate $cert -errorAction Stop
       #close the file
-      $psise.CurrentPowerShellTab.Files.remove($psise.currentfile) | Out-Null             
-  
+      $psise.CurrentPowerShellTab.Files.remove($psise.CurrentFile) | Out-Null
+
       #reopen the file
-      $psise.CurrentPowerShellTab.Files.add($filepath)  | out-null             
+      $psise.CurrentPowerShellTab.Files.add($filepath)  | Out-Null
     }
     Catch {
       Write-Warning ("Script signing failed. {0}" -f $_.Exception.message)
     }
 
-} #if code cert found 
+} #if no code cert found
 
 else {
     Write-Warning "No code signing certificate found."
